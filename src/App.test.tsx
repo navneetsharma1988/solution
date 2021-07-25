ee8d7bbc5e2a789dee3d2ef74eecc646150c0f55
navneetsharma <sharma.navneet1988@gmail.com>
2021-07-25 11:07:37 +0530

    Show header, search box and result in App component, add unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,70 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import * as useCurrencyList from './hooks/useCurrencyList';
+import { StatusEnum } from './models/status.enum';
+import { Fx } from './models/currency.model';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('./hooks/useCurrencyList');
+const mockedUseCurrencyList = useCurrencyList as jest.Mocked<typeof useCurrencyList>;
+
+const mockResultComponent = jest.fn();
+jest.mock('./components/Result/Result', () => (props: { items: Fx[] }) => {
+  mockResultComponent(props);
+  // @ts-ignore
+  return <mock-resultComponent />;
+});
+
+describe('App component', () => {
+  beforeEach(() => {
+    mockedUseCurrencyList.useCurrencyList.mockReset();
+  });
+
+  test('renders with loading text', () => {
+    mockedUseCurrencyList.useCurrencyList.mockImplementation(() => {
+      return {
+        status: StatusEnum.Loading,
+        error: null,
+        currencyList: []
+      };
+    });
+
+    render(<App />);
+
+    const loadingElement = screen.getByText('loading', { exact: false });
+    expect(loadingElement).toBeInTheDocument();
+  });
+
+  test('renders with error', () => {
+    mockedUseCurrencyList.useCurrencyList.mockImplementation(() => {
+      return {
+        status: StatusEnum.Loading,
+        error: 'mocked error',
+        currencyList: [] as Fx[]
+      };
+    });
+
+    render(<App />);
+
+    const errorElement = screen.getByText('mocked error');
+    expect(errorElement).toBeInTheDocument();
+  });
+
+  test('renders with Result component', () => {
+    mockedUseCurrencyList.useCurrencyList.mockImplementation(() => {
+      return {
+        status: StatusEnum.Loaded,
+        error: null,
+        currencyList: [{ currency: 'USD' }] as Fx[]
+      };
+    });
+
+    render(<App />);
+
+    expect(mockResultComponent).toHaveBeenCalledWith(
+      expect.objectContaining({
+        items: expect.arrayContaining([expect.objectContaining({ currency: 'USD' })])
+      })
+    );
+  });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { ChangeEvent, useState } from 'react';
+import { useCurrencyList } from './hooks/useCurrencyList';
+import { StatusEnum } from './models/status.enum';
+import { Fx } from './models/currency.model';
+import Header from './components/Header/Header';
+import Result from './components/Result/Result';
+import SearchBox from './components/SearchBox/SearchBox';
+import { QueryParamsUtil } from './utils/queryParamsUtil';
 
 function App() {
+  const { currencyList, status, error } = useCurrencyList();
+  const [searchInput, setSearchInput] = useState<string>('');
+  const [searchParams] = useState<string[]>(['currency', 'nameI18N']);
+
+  React.useEffect(() => {
+    // React Router package and useLocation & useHistory can be used but for this simple use case
+    // should avoid increasing the bundle size
+    const querySearchParams = QueryParamsUtil.getQuerySearchParams(window.location);
+    setSearchInput(querySearchParams.searchInput);
+  }, []);
+
+  function onUpdateSearchInput(event: ChangeEvent<HTMLInputElement>) {
+    const inputValue = event.target.value;
+    setSearchInput(inputValue);
+    QueryParamsUtil.updateURL(inputValue);
+  }
+
+  function search(items: Fx[]) {
+    return items.filter((item) => {
+      return searchParams.some((searchParam) => {
+        return (
+          item[searchParam as keyof Fx] &&
+          item[searchParam as keyof Fx].toString().toLowerCase().indexOf(searchInput.trim().toLowerCase()) > -1
+        );
+      });
+    });
+  }
+
+  let result;
+
+  if (error) {
+    result = <div>{error}</div>;
+  } else if (status === StatusEnum.Loading) {
+    result = <div>loading...</div>;
+  } else {
+    result = <Result items={search(currencyList)} />;
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <SearchBox searchInput={searchInput} updateSearchInput={onUpdateSearchInput} />
+      {result}
     </div>
   );
 }
